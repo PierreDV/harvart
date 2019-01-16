7e03c33dfee35fc7812ea567c1d39a98c94e0eae
PierreDV <p.verteuil@gmail.com>
2019-01-16 16:34:09 +0100

    add api key

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SC from 'node-soundcloud';
+
+const API_KEY = '81c02e10-19a3-11e9-a436-ede6838aae81';
+
+class App extends React.Component {
+  render() {
+    return(
+      <button>Sign into Soundcloud</button>
+    );
+  };
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
