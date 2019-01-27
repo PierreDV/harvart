edeba388f0e3d20729a9bd8506bf59e45325d3b8
PierreDV <p.verteuil@gmail.com>
2019-01-27 15:37:56 +0100

    move app component into seperate file

--- src/index.js
+++ src/index.js
@@ -3,90 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
-import SearchBar from './components/search_bar';
-import ImageGrid from './components/image_grid';
+import App from './components/app';
+
 import './styles/style.scss'
-
-const API_KEY = '81c02e10-19a3-11e9-a436-ede6838aae81';
-// https://github.com/harvardartmuseums/api-docs
-const BASE_URL = 'https://api.harvardartmuseums.org/'
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-    this.imageSearch = this.imageSearch.bind(this);
-  }
-
-  imageSearch(term) {
-    const PLACE_URL = `${BASE_URL}place?apikey=${API_KEY}&q=name:${term}&size=1`;
-    fetch(PLACE_URL)
-      .then(res => res.json())
-      .then(
-        (res) => {
-          console.log(res);
-          const OBJECT_URL =
-            `${BASE_URL}object?apikey=${API_KEY}&place=${res.records[0].id}&q=people.role:Artist AND imagepermissionlevel:0`
-          return fetch(OBJECT_URL)
-            .then(res => res.json())
-            .then(
-              (res) => {
-                console.log(res)
-                this.setState({
-                  isLoaded: true,
-                  result: res
-                });
-              },
-              (error) => {
-                this.setState({
-                  isLoaded: true,
-                  error
-                });
-              }
-            )
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  // serialize(obj) {
-  //   const str = [];
-  //   for (var p in obj) {
-  //     if (obj.hasOwnProperty(p)) {
-  //       str.push(`${encodeURIComponent(p)}=${encodeURIComponent(obj[p])}`);
-  //     }
-  //     return str.join("&");
-  //   }
-  // }
-
-  componentDidMount() {
-    this.imageSearch('Amsterdam');
-  }
-
-  render() {
-    const { error, isLoaded, result } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <SearchBar onSubmit={this.imageSearch}/>
-          <ImageGrid records={result.records} />
-        </div>
-      )
-    }
-  };
-}
 
 ReactDOM.render(
   <Provider store={configureStore()}>
