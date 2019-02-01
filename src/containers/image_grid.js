aa592686cb82b196e22a4642fc784ab719d243ca
PierreDV <p.verteuil@gmail.com>
2019-02-01 12:05:55 +0100

    refactored component logic

--- src/containers/image_grid.js
+++ src/containers/image_grid.js
@@ -5,15 +5,19 @@ import ImageGridItem from '../components/image_grid_item';
 class ImageGrid extends Component {
   render() {
     return(
-      <div>{ this.props.images.isFetching ? <div>Loading...</div> :
-        this.props.images.items.map(image => {
-          return(
-            <ImageGridItem
-              image_url={image.images[0].baseimageurl}
-              key={image.id}/>
-          )
-        })
-      }</div>
+      <div className='image-grid'>
+        { this.props.images.isFetching ? 
+          <div>Loading...</div> :
+          this.props.images.items.map(image => {
+            return(
+              <ImageGridItem
+                image_url={image.images[0].baseimageurl}
+                key={image.id}
+              />
+            )
+          })
+        }
+      </div>
     )
   }
 }
